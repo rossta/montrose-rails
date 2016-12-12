43d1594a20d75f3008465b385beb3b53d5c0b728
Ross Kaffenberger <rosskaff@gmail.com>
2016-12-12 08:47:02 -0500

    Make LauncherLabel a stateless component

--- test/sample/app/assets/javascripts/montrose-select/components/LauncherLabel.js
+++ test/sample/app/assets/javascripts/montrose-select/components/LauncherLabel.js
@@ -1,26 +1,24 @@
 import { h, Component } from 'preact'
 
-class LauncherLabel extends Component {
-  render({ isEnabled, isChecked, children, onChange, onLaunch }) {
-    return (
-      <label >
-        <input
-          type="checkbox"
-          checked={ isChecked }
-          onChange={ onChange } />
-        { children }
+const LauncherLabel = ({ isEnabled, isChecked, children, onChange, onLaunch }) {
+  return (
+    <label >
+      <input
+        type="checkbox"
+        checked={ isChecked }
+        onChange={ onChange } />
+      { children }
 
-        {
-          isEnabled ?
-            <span>
-              &nbsp;
-              <a href="#" onClick={ onLaunch }>Edit</a>
-            </span> :
-            ''
-        }
-      </label>
-    )
-  }
+      {
+        isEnabled ?
+          <span>
+            &nbsp;
+            <a href="#" onClick={ onLaunch }>Edit</a>
+          </span> :
+          ''
+      }
+    </label>
+  )
 }
 
 export default LauncherLabel
